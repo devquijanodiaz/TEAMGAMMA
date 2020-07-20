f40b69c5b0a907e4b3307cdf6c6599c4a646b833
Jalves33 <67927195+Jalves33@users.noreply.github.com>
2020-07-20 16:16:51 -0400

    Update pythonlab7.py

--- pythonlab7.py
+++ pythonlab7.py
@@ -1,28 +1,30 @@
-# importing the module os using the import command
+#importing os module to allow you to tun bash commands
 import os
+#displaying the curret working directory
+os.system('ls')
 
-# from the os module (os.system) use the ls bash command ('ls') to print the directories in the present working directory
-os.system("ls")
-
-# importing the module subprocess using the import command
+#importing subprocess module. The subprocess.run() can take a lot of new arguments, but those additional arguments are optional
 import subprocess
 
-# from the subprocess module (subprocess.run) use the ls bash command ('ls') to print the directories in the present working directory
-subprocess.run(["ls"])
+#Does the same thing is os.ssytem('ls')
+subprocess.run(['ls'])
 
+#the additional argument -l now lists the present working directory in long format
+subprocess.run(['ls','-l','solution'])
+
+#calling the uname command to get system information.
 command='uname'
 
-commandArgument='a'
+commandArgument='-a'
 
 print(f'Gathering systen information with command: {command} {commandArgument}')
 
+#actually running the command
 subprocess.run([command,commandArgument])
 
+
 command='ps'
-
 commandArgument='-x'
-
 # outping to screen Gathering active process information with the command=ps and commandArgument=-x
 print(f'Gathering active process information with command : {command} {commandArgument}')
-
 subprocess.run([command,commandArgument])
