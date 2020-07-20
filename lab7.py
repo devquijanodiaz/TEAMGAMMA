b26b828ea8259e7d28e9854a542ba7b5974cd01a
Jalves33 <67927195+Jalves33@users.noreply.github.com>
2020-07-19 23:57:55 -0400

    Add files via upload

---
+++ lab7.py
@@ -0,0 +1,12 @@
+# importing the module os using the import command
+import os
+
+# from the os module (os.system) use the ls bash command ('ls') to print the directories in the present working directory
+os.system("ls")
+
+# importing the module subprocess using the import command
+import subprocess
+
+# from the subprocess module (subprocess.run) use the ls bash command ('ls') to print the directories in the present working directory
+subprocess.run(["ls"])
+
