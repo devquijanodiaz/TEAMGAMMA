eeec9eb82ede0da685a1e513d0215c421b3ad52a
Jalves33 <67927195+Jalves33@users.noreply.github.com>
2020-07-20 08:28:46 -0400

    Update lab7.py

--- lab7.py
+++ lab7.py
@@ -10,3 +10,19 @@ import subprocess
 # from the subprocess module (subprocess.run) use the ls bash command ('ls') to print the directories in the present working directory
 subprocess.run(["ls"])
 
+command='uname'
+
+commandArgument='a'
+
+print(f'Gathering systen information with command: {command} {commandArgument}')
+
+subprocess.run([command,commandArgument])
+
+command='ps'
+
+commandArgument='-x'
+
+# outping to screen Gathering active process information with the command=ps and commandArgument=-x
+print(f'Gathering active process information with command : {command} {commandArgument}')
+
+subprocess.run([command,commandArgument])
